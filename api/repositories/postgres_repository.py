cc300f8e8b4f49283bb4d960a2a1b998716c2579
glen chua <glenchua91@hotmail.com>
2026-01-05 13:21:59 +0800

    fix api module not found error

--- api/repositories/postgres_repository.py
+++ api/repositories/postgres_repository.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 from typing import Any, Dict, List, Optional
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
-from api.repositories.interface_repository import DatabaseInterface
+from repositories.interface_repository import DatabaseInterface
 
 load_dotenv()
 

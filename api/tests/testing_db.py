15970c5740eee55d2981a8ca33b46cf7c77f0084
buratino91 <glenchua91@hotmail.com>
2026-01-08 16:40:48 +0800

    update requirements.txt, hostname for testdb

--- api/tests/testing_db.py
+++ api/tests/testing_db.py
@@ -12,7 +12,7 @@ from api.main import app
 async def test_db():
     # Create test db connection
     conn = await asyncpg.connect(
-        host='127.0.0.1',
+        host='postgres',
         database='test_db',
         user='test',
         password='test'

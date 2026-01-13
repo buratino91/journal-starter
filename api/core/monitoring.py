fc56f05db756306c9be5f01ddb072ae77a024906
glen chua <glenchua91@hotmail.com>
2026-01-13 14:15:27 +0800

    add histogram to monitoring.py

--- api/core/monitoring.py
+++ api/core/monitoring.py
@@ -1,9 +1,9 @@
-from prometheus_client import Counter, Gauge
+from prometheus_client import Counter, Gauge, Histogram
 
 request_counter = Counter(
     "post_requests_total",
     "Total number of requests to the endpoint",
-    ["endpoint"],
+    ["endpoint", "method", "status_code"],
 )
 
 
@@ -11,4 +11,10 @@ entry_gauge = Gauge(
     "total_number_of_entries",
     "Total number of entries in database",
     
+)
+
+request_latency = Histogram(
+    "http_request_latency_seconds",
+    "Request latency in seconds",
+    ["endpoint"]
 )

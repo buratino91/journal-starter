1050ff57af903a85dfe2b8dd7cefa0038abfd698
glen chua <glenchua91@hotmail.com>
2026-03-17 01:17:39 +0000

    Setup basic console logging

--- api/main.py
+++ api/main.py
@@ -7,6 +7,8 @@ from fastapi import FastAPI
 
 from api.routers.journal_router import router as journal_router
 
+import logging
+
 # TODO: Setup basic console logging
 # Hint: Use logging.basicConfig() with level=logging.INFO
 # Steps:
@@ -15,5 +17,15 @@ from api.routers.journal_router import router as journal_router
 # 3. Add console handler
 # 4. Test by adding a log message when the app starts
 
+logger = logging.getLogger('Journal App Logger')
+logger.setLevel(logging.INFO)
+
+# Console handler
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+logger.addHandler(ch)
+
 app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
+logger.info('App started on http://localhost:8000.')
